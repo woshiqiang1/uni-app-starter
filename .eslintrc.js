f9759056e4738517239dc8c5f61985b1286bfb76
MEGVII-INC\zoucheng02 <775106512@qq.com>
2025-03-25 17:02:53 +0800

    chore: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
   env: {
-    node: true
+    node: true,
+    es6: true
   },
   globals: {
-    uni: true
+    uni: true,
+    requirePlugin: true
   },
   extends: ['eslint:recommended', 'plugin:vue/recommended'],
+  parserOptions: {
+    ecmaVersion: 2022
+  },
   rules: {
     'vue/multi-word-component-names': 0,
     'vue/comment-directive': 0,
